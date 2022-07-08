5e53a83f29f40521a000d1470e1468988be84808
NehemieMbg <nehemie.mbg@gmail.com>
2022-07-08 11:09:38 +0000

    dBetty style modification

--- main.c
+++ main.c
@@ -9,48 +9,48 @@
  */
 int main(void)
 {
-    int len;
-    int len2;
-    unsigned int ui;
-    void *addr;
+	int len;
+	int len2;
+	unsigned int ui;
+	void *addr;
 
-    len = _printf("Let's try to printf a simple sentence.\n");
-    len2 = printf("Let's try to printf a simple sentence.\n");
+	len = _printf("Let's try to printf a simple sentence.\n");
+	len2 = printf("Let's try to printf a simple sentence.\n");
 	printf("\n");
-    ui = (unsigned int)INT_MAX + 1024;
-    addr = (void *)0x7ffe637541f0;
-    _printf("Length:[%d, %i]\n", len, len);
-    printf("Length:[%d, %i]\n", len2, len2);
+	ui = (unsigned int)INT_MAX + 1024;
+	addr = (void *)0x7ffe637541f0;
+	_printf("Length:[%d, %i]\n", len, len);
+	printf("Length:[%d, %i]\n", len2, len2);
 	printf("\n");
-    _printf("Negative:[%d]\n", -762534);
-    printf("Negative:[%d]\n", -762534);
+	_printf("Negative:[%d]\n", -762534);
+	printf("Negative:[%d]\n", -762534);
 	printf("\n");
-    _printf("Unsigned:[%u]\n", ui);
-    printf("Unsigned:[%u]\n", ui);
+	_printf("Unsigned:[%u]\n", ui);
+	printf("Unsigned:[%u]\n", ui);
 	printf("\n");
-    _printf("Unsigned octal:[%o]\n", ui);
-    printf("Unsigned octal:[%o]\n", ui);
+	_printf("Unsigned octal:[%o]\n", ui);
+	printf("Unsigned octal:[%o]\n", ui);
 	printf("\n");
-    _printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-    printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+	_printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+	printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
 	printf("\n");
-    _printf("Character:[%c]\n", 'H');
-    printf("Character:[%c]\n", 'H');
+	_printf("Character:[%c]\n", 'H');
+	printf("Character:[%c]\n", 'H');
 	printf("\n");
-    _printf("String:[%s]\n", "I am a string !");
-    printf("String:[%s]\n", "I am a string !");
+	_printf("String:[%s]\n", "I am a string !");
+	printf("String:[%s]\n", "I am a string !");
 	printf("\n");
-    _printf("Address:[%p]\n", addr);
-    printf("Address:[%p]\n", addr);
+	_printf("Address:[%p]\n", addr);
+	printf("Address:[%p]\n", addr);
 	printf("\n");
-    len = _printf("Percent:[%%]\n");
-    len2 = printf("Percent:[%%]\n");
+	len = _printf("Percent:[%%]\n");
+	len2 = printf("Percent:[%%]\n");
 	printf("\n");
-    _printf("Len:[%d]\n", len);
-    printf("Len:[%d]\n", len2);
+	_printf("Len:[%d]\n", len);
+	printf("Len:[%d]\n", len2);
 	printf("\n");
-    /**_printf("Unknown:[%r]\n");
-    *printf("Unknown:[%r]\n");*/
+	/**_printf("Unknown:[%r]\n");
+	*printf("Unknown:[%r]\n");*/
 	printf("\n");
-    return (0);
+	return (0);
 }
