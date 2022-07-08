ab51523bccd4f290639d6a257188d2e932f39956
NehemieMbg <nehemie.mbg@gmail.com>
2022-07-08 08:58:09 +0000

    hotfix

---
+++ _printf.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * _printf - Produces output arccording to a format
+ * @format: is the pointer that point to the first character of a string
+ *
+ * Return: the number of character printed
+ */
+int _printf(const char *format, ...)
+{
+    prt_f prt[] = {
+        {'c', print_char},
+        {'s', print_string},
+        {'\0', NULL},
+    };
+    int i = 0, count = 0, j;
+    va_list lst;
+    va_start(lst, format);
+    while (format[i])
+    {
+        j = 0;
+        if (format[i] == '%')
+        {
+            while (prt[j].c)
+            {
+                if (prt[j].c == format[i + 1])
+                {
+                    count += prt[j].f(lst);
+                }
+                j++;
+            }
+            i++;
+        }
+        else
+        {
+            write(STDOUT_FILENO, &format[i], 1);
+            count += 1;
+        }
+        i++;
+    }
+    va_end(lst);
+    return (count);
+}
